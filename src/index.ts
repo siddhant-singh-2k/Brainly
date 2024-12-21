506e65d3b075f20bf47cf908850d7dc7c0c9bbd8
Siddhant <onlyeeofficial@gmail.com>
2024-12-21 16:15:48 +0530

    Adding routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-import express from 'express';
+import mongoose  from "mongoose";
+import jwt from 'jsonwebtoken';
+
+import express from "express";
+const app = express();
+
+
+
+app.get('/api/v1/signup',function (req,res) {
+
+})
+
+app.get('/api/v1/signin',function (req,res) {
+
+})
+app.get('/api/v1/content',function (req,res) {
+
+})
+app.get('/api/v1/content',function (req,res) {
+
+})
+app.get('/api/v1/share',function (req,res) {
+
+})
+app.get('/api/v1/shareLink',function (req,res) {
+
+})
+
+
